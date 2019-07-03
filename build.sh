6005ed10413aad34b56969bf14135e0bffec9b50
Alexandru Bagu <a.bagu@software-dep.net>
2019-07-03 17:23:50 +0300

    added missing libm reference

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-clang source/image.h source/etcpack.cxx source/image.cxx source/etcdec.cxx -lstdc++
+rm etcpack
+clang source/image.h source/etcpack.cxx source/image.cxx source/etcdec.cxx -lstdc++ -lm
 mv a.out etcpack
